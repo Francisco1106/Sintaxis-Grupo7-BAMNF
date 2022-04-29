480f1a045ad430f1b139babf2adca76aad6b168d
mijHwang <102636178+mijHwang@users.noreply.github.com>
2022-04-28 21:36:37 -0300

    Actalizacion punto C

--- puntoC.c
+++ puntoC.c
@@ -7,21 +7,38 @@
 
 
 
-int toMayus()
+void toMayus(char* cadena)
 {
-    int c;
-    c=getchar();
+    int i=0;
 
-    char recp[] = "";
-    while(c!=EOF)
+    while(*(cadena+i) !='\0')
     {
+        //if(minuscula) // sumar 32 al asc o directamente cambiar a mayuscula con switch
+        if(*(cadena+i) >= 97 && *(cadena+i) <= 122)
+        {
+          *(cadena+i) = *(cadena+i) - 32;
+        }
 
-        char i =  toupper(c);
-
-        strncat(recp, &i, 1);
-
-        c=getchar();
+       i++;
     }
-
-    return recp;
 }
+
+    /*
+
+    char temp[] = *cadena;
+    int i=0;
+    while(temp[i] != '\0')
+    {
+        //if(minuscula) // sumar 32 al asc o directamente cambiar a mayuscula con switch
+        if(temp[i] >= 97 && temp[i] <= 122)
+        {
+          temp[i] = temp[i] - 32;
+        }
+
+        i++;
+    }
+    cadena = &temp[0];
+
+
+}
+*/
